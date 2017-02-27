7111e6ed39d01da1f60c8c114d707bd89d24a640
farhanarif95 <farhanarif95@gmail.com>
2017-02-27 23:26:11 +0530

    My First Commit

---
+++ fidoCore/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using fidoCore.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace fidoCore.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
