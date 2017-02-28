91266c4f625deb4ffbb0fdbb72d04c3f5adfb8c4
farhanarif95 <farhanarif95@gmail.com>
2017-02-28 11:24:52 +0530

    signup page done

--- fidoCore/Views/SignupPage.xaml.cs
+++ fidoCore/Views/SignupPage.xaml.cs
@@ -20,9 +20,9 @@ namespace fidoCore.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class WelcomePage : Page
+    public sealed partial class SignupPage : Page
     {
-        public WelcomePage()
+        public SignupPage()
         {
             this.InitializeComponent();
         }
