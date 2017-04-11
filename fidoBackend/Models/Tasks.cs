81e49a305fe2b7e27d327b762ddac4039cfd6aed
Abdul Muhaymin Pary <mampary@hotmail.com>
2017-04-11 15:47:25 +0530

    Added Tasks Model

---
+++ fidoBackend/Models/Tasks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fidoBackend.Models
+{
+    public class Tasks
+    {
+        public string id { get; set; }
+        public string projectId { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string assidedTo { get; set; }
+        public string status { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+    }
+}
